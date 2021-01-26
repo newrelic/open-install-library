51884dd6917092c31b8957651c8c901611c173b8
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-01-26 15:09:12 +0100

    refactor(debian): moved rs initiate

--- test/deploy/linux/mongodb/install/debian/roles/configure/templates/commands.js
+++ test/deploy/linux/mongodb/install/debian/roles/configure/templates/commands.js
@@ -1,4 +1,4 @@
+rs.inititate();
 use admin;
-rs.inititate()
 db.createRole({role: "listCollections", privileges: [{resource: {db:"",collection:""},actions: ["listCollections"]}],roles: []})
 db.createUser({user: "username", pwd: "password", roles: ["clusterMonitor","listCollections"]})
