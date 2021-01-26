4ce6554f3daef1b7c01029b1fb39de76f608ea2f
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-01-26 12:29:11 +0100

    refactor(debian): implement changes for replication

--- test/deploy/linux/mongodb/install/debian/roles/configure/templates/commands.js
+++ test/deploy/linux/mongodb/install/debian/roles/configure/templates/commands.js
@@ -1,3 +1,4 @@
 use admin;
 db.createRole({role: "listCollections", privileges: [{resource: {db:"",collection:""},actions: ["listCollections"]}],roles: []})
 db.createUser({user: "username", pwd: "password", roles: ["clusterMonitor","listCollections"]})
+rs.inititate()
