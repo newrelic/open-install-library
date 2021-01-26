6035f876d24e426adbf6f5ea36ae08e7e0d45269
setamyDG <setamy.dg@gmail.com>
2021-01-26 15:28:48 +0100

    fix(mongo-rhel): add new test file

--- test/deploy/linux/mongodb/install/rhel/roles/configure/templates/test.js
+++ test/deploy/linux/mongodb/install/rhel/roles/configure/templates/test.js
@@ -1,4 +1,3 @@
-rs.initiate() 
 use admin;
 db.createRole({role: "listCollections", privileges: [{resource: {db:"admin",collection:""},actions: ["listCollections"]}],roles: []})
 db.createUser({user: "username", pwd: "password", roles: ["clusterMonitor","listCollections"]})
