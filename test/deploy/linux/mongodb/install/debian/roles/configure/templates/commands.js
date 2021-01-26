6ebab460286b5a3d127bb13111b9ed0a75756637
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-01-26 15:31:51 +0100

    refactor(debian): split script files for mongodb

--- test/deploy/linux/mongodb/install/debian/roles/configure/templates/commands.js
+++ test/deploy/linux/mongodb/install/debian/roles/configure/templates/commands.js
@@ -1,4 +1,3 @@
-rs.inititate();
 use admin;
 db.createRole({role: "listCollections", privileges: [{resource: {db:"",collection:""},actions: ["listCollections"]}],roles: []})
 db.createUser({user: "username", pwd: "password", roles: ["clusterMonitor","listCollections"]})
