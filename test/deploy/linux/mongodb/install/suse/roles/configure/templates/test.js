16b53a2ab0dfe40e0be3e18a4a5e4d544f6a8fed
setamyDG <setamy.dg@gmail.com>
2021-01-27 13:15:14 +0100

    fix(suse-mongodb): update script files

---
+++ test/deploy/linux/mongodb/install/suse/roles/configure/templates/test.js
@@ -0,0 +1,3 @@
+use admin;
+db.createRole({role: "listCollections", privileges: [{resource: {db:"admin",collection:""},actions: ["listCollections"]}],roles: []})
+db.createUser({user: "username", pwd: "password", roles: ["clusterMonitor","listCollections"]}) 
