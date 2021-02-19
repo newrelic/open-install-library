a7bc0df6addc8597ecb69b9330eb85fd49ecdf35
setamyDG <setamy.dg@gmail.com>
2021-02-19 16:27:43 +0100

    fix: delete all comments from .sh file

--- test/deploy/linux/kafka/install/rhel/roles/configure/templates/kafka-run-class.sh
+++ test/deploy/linux/kafka/install/rhel/roles/configure/templates/kafka-run-class.sh
@@ -4,7 +4,7 @@ then
   exit 1
 fi
 
-# CYGWIN == 1 if Cygwin is detected, else 0.
+
 if [[ $(uname -a) =~ "CYGWIN" ]]; then
   CYGWIN=1
 else
@@ -15,7 +15,7 @@ if [ -z "$INCLUDE_TEST_JARS" ]; then
   INCLUDE_TEST_JARS=false
 fi
 
-# Exclude jars not necessary for running commands.
+
 regex="(-(test|test-sources|src|scaladoc|javadoc)\.jar|jar.asc)$"
 should_include_file() {
   if [ "$INCLUDE_TEST_JARS" = true ]; then
@@ -42,7 +42,7 @@ if [ -z "$SCALA_BINARY_VERSION" ]; then
   SCALA_BINARY_VERSION=$(echo $SCALA_VERSION | cut -f 1-2 -d '.')
 fi
 
-# run ./gradlew copyDependantLibs to get all dependant jars in a local dir
+
 shopt -s nullglob
 if [ -z "$UPGRADE_KAFKA_STREAMS_TEST_VERSION" ]; then
   for dir in "$base_dir"/core/build/dependant-libs-${SCALA_VERSION}*;
@@ -144,7 +144,7 @@ do
   fi
 done
 
-# classpath addition for release
+
 for file in "$base_dir"/libs/*;
 do
   if should_include_file "$file"; then
@@ -165,55 +165,55 @@ if [ -z "$CLASSPATH" ] ; then
   exit 1
 fi
 
-# JMX settings
+
 if [ -z "$KAFKA_JMX_OPTS" ]; then
   KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=localhost -Djava.net.preferIPv4Stack=true"
 fi
 
-# JMX port to use
+
 if [  $JMX_PORT ]; then
   KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.port=$JMX_PORT "
 fi
 
-# Log directory to use
+
 if [ "x$LOG_DIR" = "x" ]; then
   LOG_DIR="$base_dir/logs"
 fi
 
-# Log4j settings
+
 if [ -z "$KAFKA_LOG4J_OPTS" ]; then
-  # Log to console. This is a tool.
+  
   LOG4J_DIR="$base_dir/config/tools-log4j.properties"
-  # If Cygwin is detected, LOG4J_DIR is converted to Windows format.
+  
   (( CYGWIN )) && LOG4J_DIR=$(cygpath --path --mixed "${LOG4J_DIR}")
   KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${LOG4J_DIR}"
 else
-  # create logs directory
+  
   if [ ! -d "$LOG_DIR" ]; then
     mkdir -p "$LOG_DIR"
   fi
 fi
 
-# If Cygwin is detected, LOG_DIR is converted to Windows format.
+
 (( CYGWIN )) && LOG_DIR=$(cygpath --path --mixed "${LOG_DIR}")
 KAFKA_LOG4J_OPTS="-Dkafka.logs.dir=$LOG_DIR $KAFKA_LOG4J_OPTS"
 
-# Generic jvm settings you want to add
+
 if [ -z "$KAFKA_OPTS" ]; then
   KAFKA_OPTS=""
 fi
 
-# Set Debug options if enabled
+
 if [ "x$KAFKA_DEBUG" != "x" ]; then
 
-    # Use default ports
+    
     DEFAULT_JAVA_DEBUG_PORT="5005"
 
     if [ -z "$JAVA_DEBUG_PORT" ]; then
         JAVA_DEBUG_PORT="$DEFAULT_JAVA_DEBUG_PORT"
     fi
 
-    # Use the defaults if JAVA_DEBUG_OPTS was not set
+    
     DEFAULT_JAVA_DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=${DEBUG_SUSPEND_FLAG:-n},address=$JAVA_DEBUG_PORT"
     if [ -z "$JAVA_DEBUG_OPTS" ]; then
         JAVA_DEBUG_OPTS="$DEFAULT_JAVA_DEBUG_OPTS"
@@ -223,20 +223,20 @@ if [ "x$KAFKA_DEBUG" != "x" ]; then
     KAFKA_OPTS="$JAVA_DEBUG_OPTS $KAFKA_OPTS"
 fi
 
-# Which java to use
+
 if [ -z "$JAVA_HOME" ]; then
   JAVA="java"
 else
   JAVA="$JAVA_HOME/bin/java"
 fi
 
-# Memory options
+
 if [ -z "$KAFKA_HEAP_OPTS" ]; then
   KAFKA_HEAP_OPTS="-Xmx256M"
 fi
 
-# JVM performance options
-# MaxInlineLevel=15 is the default since JDK 14 and can be removed once older JDKs are no longer supported
+
+
 if [ -z "$KAFKA_JVM_PERFORMANCE_OPTS" ]; then
   KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent -XX:MaxInlineLevel=15 -Djava.awt.headless=true"
 fi
@@ -265,20 +265,13 @@ while [ $# -gt 0 ]; do
   esac
 done
 
-# GC options
+
 GC_FILE_SUFFIX='-gc.log'
 GC_LOG_FILE_NAME=''
 if [ "x$GC_LOG_ENABLED" = "xtrue" ]; then
   GC_LOG_FILE_NAME=$DAEMON_NAME$GC_FILE_SUFFIX
 
-  # The first segment of the version number, which is '1' for releases before Java 9
-  # it then becomes '9', '10', ...
-  # Some examples of the first line of `java --version`:
-  # 8 -> java version "1.8.0_152"
-  # 9.0.4 -> java version "9.0.4"
-  # 10 -> java version "10" 2018-03-20
-  # 10.0.1 -> java version "10.0.1" 2018-04-17
-  # We need to match to the end of the line to prevent sed from printing the characters that do not match
+
   JAVA_MAJOR_VERSION=$("$JAVA" -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
   if [[ "$JAVA_MAJOR_VERSION" -ge "9" ]] ; then
     KAFKA_GC_LOG_OPTS="-Xlog:gc*:file=$LOG_DIR/$GC_LOG_FILE_NAME:time,tags:filecount=10,filesize=102400"
@@ -287,15 +280,13 @@ if [ "x$GC_LOG_ENABLED" = "xtrue" ]; then
   fi
 fi
 
-# Remove a possible colon prefix from the classpath (happens at lines like `CLASSPATH="$CLASSPATH:$file"` when CLASSPATH is blank)
-# Syntax used on the right side is native Bash string manipulation; for more details see
-# http://tldp.org/LDP/abs/html/string-manipulation.html, specifically the section titled "Substring Removal"
+
 CLASSPATH=${CLASSPATH#:}
 
-# If Cygwin is detected, classpath is converted to Windows format.
+
 (( CYGWIN )) && CLASSPATH=$(cygpath --path --mixed "${CLASSPATH}")
 
-# Launch mode
+
 if [ "x$DAEMON_MODE" = "xtrue" ]; then
   nohup "$JAVA" $KAFKA_HEAP_OPTS $KAFKA_JVM_PERFORMANCE_OPTS $KAFKA_GC_LOG_OPTS $KAFKA_JMX_OPTS $KAFKA_LOG4J_OPTS -cp "$CLASSPATH" $KAFKA_OPTS "$@" > "$CONSOLE_OUTPUT_FILE" 2>&1 < /dev/null &
 else
