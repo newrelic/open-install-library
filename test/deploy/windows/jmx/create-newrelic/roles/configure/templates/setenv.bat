05822dec559bc9c3c9b996ba35a036dbb2da6cc2
setamyDG <setamy.dg@gmail.com>
2021-03-26 12:10:20 +0100

    fix: fix setenv file

--- test/deploy/windows/jmx/create-newrelic/roles/configure/templates/setenv.bat
+++ test/deploy/windows/jmx/create-newrelic/roles/configure/templates/setenv.bat
@@ -1,1 +1,1 @@
-set CATALINA_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false
+set JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false
