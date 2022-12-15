cbb6cd2ae568dc243544ce8a2203c61d9f5ee0a0
Julien Erard <jerard@newrelic.com>
2022-12-15 08:42:50 -0800

    chore: adding summary notification to terminate old lambda

--- docs/test-framework/terminate-ec2-missing-owning-team.py
+++ docs/test-framework/terminate-ec2-missing-owning-team.py
@@ -27,7 +27,7 @@ def lambda_handler(event, context):
         if len(instances_to_delete) > 0:
             for instance in instances_to_delete:
                 instance_count = instance_count+1
-                text = 'Deleting from region:' +region_name +' instance with ID:' +str(instance.id) +" having tags:" +json.dumps(instance.tags) +' key_name:' +instance.key_name +' launch_time:' +str(instance.launch_time)
+                text = 'Terminating from region:' +region_name +' instance with ID:' +str(instance.id) +" having tags:" +json.dumps(instance.tags) +' key_name:' +instance.key_name +' launch_time:' +str(instance.launch_time)
                 instance.terminate()
                 summary = summary +text +'\n'
                 print(text)
